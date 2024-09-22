d3f231a6f951c52beccb8d3c94c152db9db2ecd0
lelyan <lelyanravida@gmail.com>
2024-09-22 20:18:14 +0200

    jeu terminé

--- apagnan.js
+++ apagnan.js
@@ -2,30 +2,35 @@ let Pierre = document.getElementById("Pierre")
 let Feuille = document.getElementById("Feuille")
 let Ciseaux = document.getElementById("Ciseaux")
 let jeux = document.getElementById("jeu")
-/*const button = document.querySelectorAll("#box button")
-console.log(button)*/
-Pierre.addEventListener("click",jeu("Pierre"))
-Feuille.addEventListener("click",jeu("Feuille"))
-Ciseaux.addEventListener("click",jeu("Ciseaux"))
+
+let choixHu = document.getElementById("choixHu")
+let choixbo = document.getElementById("choix")
+Pierre.addEventListener("click", function() {jeu("Pierre")})
+Feuille.addEventListener("click", function() {jeu("Feuille")})
+Ciseaux.addEventListener("click", function() {jeu("Ciseaux")})
+
+
 
 function jeu(choix){
     let tab = ["Pierre","Feuille","Ciseaux"]
     let bot = Math.floor(Math.random()*3)
     let choixbot = tab[bot] 
-    if(choixbot == "Pierre" && choix == "Feuille"){
-        jeux.textContent("t'as gagné")
+    choixbo.textContent = choixbot
+    choixHu.textContent = choix
+    if(choixbot == tab[0] && choix == "Feuille"){
+        jeux.textContent ="t'as gagné"
     }
-    if(choixbot == "Ciseaux" && choix == "Pierre"){
-        jeux.textContent("t'as gagné")
+    else if(choixbot == "Ciseaux" && choix == "Pierre"){
+        jeux.textContent = "t'as gagné"
     }
-    if(choixbot == "Feuille" && choix == "Ciseaux"){
-        jeux.textContent("t'as gagné")
+    else if(choixbot == "Feuille" && choix == "Ciseaux"){
+        jeux.textContent = "t'as gagné"
     }
-    if(choixbot == choix){
-        jeux.textContent("égalité")
+    else if(choixbot == choix){
+        jeux.textContent = "égalité"
     }
     else{
-        jeux.textContent("t'as perdu")
+        jeux.textContent = "t'as perdu" 
     }
 }
 
